a8e8b8309ab3257185caa1c7df5a824cbb544fb2
Verta-master <vertyagina@gmail.com>
2017-11-23 22:58:47 +0600

    First screen

---
+++ README.md
@@ -0,0 +1,2 @@
+Landing page
+Adaptive layout for mobile, tablet and desktop
---
+++ js/script.js
@@ -0,0 +1,15 @@
+var navMain = document.querySelector(".menu");
+var navToggle = document.querySelector(".menu__btn");
+var overlay = document.querySelector(".header");
+
+navToggle.addEventListener("click", function() {
+  if (navMain.classList.contains("menu--closed")) {
+    navMain.classList.remove("menu--closed");
+    navMain.classList.add("menu--opened");
+    overlay.classList.add("overlay");
+  } else {
+    navMain.classList.add("menu--closed");
+    navMain.classList.remove("menu--opened");
+    overlay.classList.remove("overlay");
+  }
+});
